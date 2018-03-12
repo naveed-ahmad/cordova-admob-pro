7cdc2162bdf927b402aead6b7d71cb40dc9bd4c7
Naveed Ahmad <naveedahmada036@gmail.com>
2018-03-12 05:19:18 +0500

    renamed the project, and added test keys back

--- src/android/AdMobPlugin.java
+++ src/android/AdMobPlugin.java
@@ -47,6 +47,9 @@ public class AdMobPlugin extends GenericAdPlugin {
   private static final String OPT_FACEBOOK = "Facebook";
   private static final String OPT_MOBFOX = "MobFox";
 
+  private static final String TEST_BANNER_ID = "ca-app-pub-3940256099942544/6300978111";
+  private static final String TEST_INTERSTITIAL_ID = "ca-app-pub-3940256099942544/1033173712";
+  private static final String TEST_REWARDVIDEO_ID = "ca-app-pub-3940256099942544/5224354917";
 
   private AdSize adSize = AdSize.SMART_BANNER;
 
